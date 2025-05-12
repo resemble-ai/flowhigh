ed1f0ee686f2dcf557af26f9b35681e9d3b1caa9
JeremyCCHsu <jeremycchsu@gmail.com>
2025-05-12 04:15:47 +0000

    expose timesteps

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ import gradio as gr
 model = FlowHighSR.from_local("checkpoints", device="cuda")
 
 
-def generate(audio, sr_out):
+def generate(audio, sr_out, timestep):
     sr_in, audio = audio
     print(sr_in)
     print(audio)
     wav = model.generate(
-        audio, sr_in, sr_out
+        audio, sr_in, sr_out, timestep=timestep,
     )
     return sr_out, wav.detach().cpu().squeeze(0).numpy()
 
@@ -20,6 +20,7 @@ demo = gr.Interface(
     [
         gr.Audio(sources="upload", type="numpy", label="Input audio file"),
         gr.Radio([16000, 22050, 24000, 32000, 44100, 48000], value=48000),
+        gr.Slider(1, 50, step=1, label="#steps", value=1),
     ],
     "audio",
 )
